7e82b281f33a0324295b18c76e6acbeae1cd1b43
Tony MacDonell <tony@teknision.com>
2021-05-27 14:53:16 -0400

    Added the ability to build all themes as an RPM file.

---
+++ scripts/rpm-build-themes.js
@@ -0,0 +1,36 @@
+const detect = require('./keycloak-detect-authorables');
+const buildRpm = require('rpm-builder');
+const config = require('../package.json');
+
+const KEYCLOAK_THEMES_DIR = '/opt/jboss/keycloak/themes';
+
+function filename(path) {
+	const s = path.split('/');
+	return s[s.length-1];
+}
+
+/**
+ * Will locate all themes in the src folder and 
+ * package their contents into an RPM for 
+ * installation into Keycloak.
+ */
+function packageThemes() {
+	const authorables = detect();
+	const themes = authorables.filter(a => a.type === 'theme').map(a => a.path);
+	buildRpm({
+		rpmDest: './build',
+		tempDir:'./build/tmp',
+		name: config.name,
+		version: process.env.BUILDID || config.version,
+		release: '1',
+		buildArch: 'noarch',
+		files:themes.map(theme => {
+			const name = filename(theme);
+			return ({cwd: theme, src: '**/*.*', dest: `${KEYCLOAK_THEMES_DIR}/${name}`})
+		})
+	}, function(err, rpm) {
+		if(err) console.log(err || rpm);
+	});
+}
+
+packageThemes();
